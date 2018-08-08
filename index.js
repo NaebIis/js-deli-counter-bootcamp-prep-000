2790a563e921f3e4edffa2d9a9710c32c52fb0f3
John Grubbs <johnmlowery@gmail.com>
2018-08-08 22:55:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
+function nowServing(katzDeliLine) {
+  katzDeliLine.pop()
+  return
+}
